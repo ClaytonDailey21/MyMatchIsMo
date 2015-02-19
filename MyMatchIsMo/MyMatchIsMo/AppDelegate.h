c14f21e740a8c668e4c16e5e7fb48ad78a468da2
ClaytonDailey <ClaytonDailey21@gmail.com>
2015-02-19 21:13:30 +0800

    Init Repo

---
+++ MyMatchIsMo/MyMatchIsMo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMatchIsMo
+//
+//  Created by Dailey on 2015/2/19.
+//  Copyright (c) 2015年 ClaytonDailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMatchIsMo/MyMatchIsMo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMatchIsMo
+//
+//  Created by Dailey on 2015/2/19.
+//  Copyright (c) 2015年 ClaytonDailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
