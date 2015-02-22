cd2a86c361e7bcc14d45e9e6a57fdd35d061223e
ClaytonDailey <ClaytonDailey21@gmail.com>
2015-02-22 21:50:48 +0800

    1. Just a save point.

--- MyMatchIsMo/MyMatchIsMo/CardGameViewController.m
+++ MyMatchIsMo/MyMatchIsMo/CardGameViewController.m
@@ -14,14 +14,10 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
