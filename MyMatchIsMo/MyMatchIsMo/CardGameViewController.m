b3ce215861b4463fc6a46e6c541f05bee048fd00
ClaytonDailey <ClaytonDailey21@gmail.com>
2015-02-23 11:20:18 +0800

    1. Fix error use cardfornt as Image, it should using as background.
    2.Implement flips AND flipsCount

--- MyMatchIsMo/MyMatchIsMo/CardGameViewController.m
+++ MyMatchIsMo/MyMatchIsMo/CardGameViewController.m
@@ -9,14 +9,31 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 
 @end
 
 @implementation CardGameViewController
 
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
+}
+
 - (IBAction)touchCardButton:(UIButton *)sender {
     
-    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+        [sender setTitle:@"♠︎2" forState:UIControlStateNormal];
+    }
+    
+    self.flipCount++;
 }
 
 
